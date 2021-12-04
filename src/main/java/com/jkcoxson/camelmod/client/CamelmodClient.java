61b83856ba615257b31104218f776508e1d4de7a
jkcoxson <jkcoxson@gmail.com>
2021-12-03 23:39:52 -0700

    Change loom version

--- src/main/java/com/jkcoxson/camelmod/client/CamelmodClient.java
+++ src/main/java/com/jkcoxson/camelmod/client/CamelmodClient.java
@@ -1,3 +1,9 @@
+package com.jkcoxson.camelmod.client;
+
 @net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
 public class CamelmodClient {
+    // Stop because this is a server side mod
+    public CamelmodClient() {
+        throw new RuntimeException("Tried to instantiate a client-only class on a server mod!");
+    }
 }
